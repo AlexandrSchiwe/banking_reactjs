f6b12c8b073cdd0b6ba9b166cefa767b833aa0f9
githubsupercode <alex.supercode@gmail.com>
2022-06-17 00:07:57 +0200

    commit on 'main'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState(0);
+
+  const handleOnChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  const handleSubtractValue = () => {
+    if (input)
+      setCount(count - parseInt(input, 10));
+  }
+
+  const handleAddValue = () => {
+    if (input)
+      setCount(count + parseInt(input, 10));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input type="text" value={input} onChange={handleOnChange} name="" id="" />
+        <button onClick={handleSubtractValue}>Ausgaben</button>
+        <button onClick={handleAddValue}>Einnahmen</button>
+        {/* <button onClick={() => setCount(count + 1)}>Einnahmen</button> */}
+        <p>{count}€</p>
+        {/* <button onClick={() => setCount(count - 1)}>Ausgaben</button>
+        <button onClick={() => setCount(count - count)}>Reset</button> */}
+      </div>
     </div>
   );
 }
